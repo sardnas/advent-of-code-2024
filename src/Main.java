38e9d5c25d58b8170c3795296b8a8ffee5254151
Sandra Reinecke <sandra.reinecke@consid.se>
2024-12-07 17:42:22 +0100

    started working on day 3 part 2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 import Themes.HistorianHysteria;
+import Themes.MullItOver;
 import Themes.RedNosedReports;
 
 public class Main {
     public static void main(String[] args) {
         HistorianHysteria historianHysteria = new HistorianHysteria();
-        System.out.printf("Day 1 Part one: %d \n", historianHysteria.getTotalDistance());
-        System.out.printf("Day 1 Part two: %d \n", historianHysteria.getSimilarityScore());
+        //System.out.printf("Day 1 Part one: %d \n", historianHysteria.getTotalDistance());
+        //System.out.printf("Day 1 Part two: %d \n", historianHysteria.getSimilarityScore());
         RedNosedReports redNosedReports = new RedNosedReports();
-        System.out.printf("Day 2 Part one: %d \n", redNosedReports.getSafeReports());
-        System.out.printf("Day 2 Part two: %d \n", redNosedReports.getSafeReportsWithProblemDampener());
+        //System.out.printf("Day 2 Part one: %d \n", redNosedReports.getSafeReports());
+        //System.out.printf("Day 2 Part two: %d \n", redNosedReports.getSafeReportsWithProblemDampener());
+        MullItOver mullItOver = new MullItOver();
+        System.out.printf("Day 3 Part one: %d \n", mullItOver.getMulSum());
+        System.out.printf("Day 3 Part two: %d \n", mullItOver.getReEnabledMulSum());
     }
 }
--- src/Themes/MullItOver.java
+++ src/Themes/MullItOver.java
@@ -13,8 +13,10 @@ import java.util.regex.Pattern;
 public class MullItOver {
     String input;
     String testInput;
-    String regex = "mul\\(\\d+,\\d+\\)";
-    Pattern pattern = Pattern.compile(regex);
+    String matchMulRegex = "mul\\(\\d+,\\d+\\)";
+    String matchDontMulRegex = "mul\\(\\d+,\\d+\\)";
+
+    Pattern pattern = Pattern.compile(matchMulRegex);
     public MullItOver(){
         try {
             URLReader reader = new URLReader();
@@ -32,16 +34,39 @@ public class MullItOver {
     }
 
     public int getMulSum(){
-        List<String> listOfMuls = getMuls();
+        return getSum(getMuls());
+    }
+    public int getReEnabledMulSum(){
+        List<String> allMulls = getMuls();
+        List<String> dontMuls = getDontMuls();
+        List<String> result = removeDuplicates(allMulls, dontMuls);
+        return getSum(result);
+    }
+
+    private int getSum(List<String> list){
         int sum = 0;
-        for(String mul : listOfMuls){
+        for(String mul : list){
             sum = sum + getProduct(mul);
         }
-        return 0;
+        return sum;
+    }
+    private List<String> removeDuplicates(List<String> list, List<String> duplicates){
+        list.removeAll(duplicates);
+        return list;
+    }
+    private List<String> getDontMuls(){
+        Pattern pattern = Pattern.compile(matchDontMulRegex);
+        Matcher matcher = pattern.matcher(testInput);
+
+        List<String> matches = new ArrayList<>();
+        while (matcher.find()) {
+            matches.add(matcher.group());
+        }
+        return matches;
     }
 
     private List<String> getMuls(){
-        Pattern pattern = Pattern.compile(regex);
+        Pattern pattern = Pattern.compile(matchMulRegex);
         Matcher matcher = pattern.matcher(testInput);
 
         List<String> matches = new ArrayList<>();
